eda6e726eeebdb5a8a8d42a2022720ae5d296554
Ly Tran <tranthilyly.khtnk15@gmail.com>
2024-10-25 22:29:57 +0300

    config: Add prettier, typescript and tailwindCSS

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      Hello! My name is <strong>LiLy</strong> and I am a software engineer.
+    </div>
+  );
+};
+
+export default HomePage;
